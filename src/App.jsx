61aaebf11114dc873aa86f44263f4d072f097a80
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-03-04 03:50:27 -0800

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="w-full h-screen  flex">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/details/1" element={<Details />} />
+        <Route path="/details/:id" element={<Details />} />
       </Routes>
     </div>
   );
