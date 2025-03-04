9d351c2adfbb34f10e49e6dc8eec531236db9a99
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-03-04 03:42:40 -0800

    made navbar functional

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Nav from "./Nav";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { contextProduct } from "../utils/Context";
+import Loading from "./Loading";
+import instance from "../utils/api";
 
 function Home() {
-  return (
+  const [products] = useContext(contextProduct);
+  const { search } = useLocation();
+
+  const [filteredproduct, setfilteredproduct] = useState(null);
+  let category = decodeURIComponent(search.split("=")[1]);
+
+  const getproductcategory = async () => {
+    try {
+      const { data } = await instance.get(`/products/category/${category}`);
+      setfilteredproduct(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!filteredproduct) setfilteredproduct(products);
+    if (category != "undefined") getproductcategory();
+    console.log(filteredproduct);
+  }, [category, products]);
+  return products ? (
     <>
       <Nav />
-      <div className="w-[85%]  p-10 flex flex-wrap overflow-x-hidden overflow-y-auto ">
-        <Link
-          to="/details/1"
-          className="card w-[20%] h-[32vh] flex flex-col  justify-center items-center border border-black/10 rounded-lg p-2 shadow-sm "
-        >
-          <div className="object-cover w-[80%] h-[35vh] mb-1 hover:scale-110 flex justify-center">
-            <img
-              className="w-[90%] h-[150px] lg:h-[250px]"
-              src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-            />
-          </div>
-          <h1 className="font-semibold whitespace-nowrap">
-            Classic Black Electric Bicycle
-          </h1>
-        </Link>
+      <div className="w-[85%]  p-10 flex flex-wrap overflow-x-hidden overflow-y-auto  ">
+        {filteredproduct &&
+          filteredproduct.map((item, index) => (
+            <Link
+              key={index}
+              to={`/details/${item.id}`}
+              className="card w-[19%] h-[32vh] flex flex-col  justify-center items-center border border-black/10 rounded-lg p-2 shadow-sm mb-4 mr-2 "
+            >
+              <div className="object-cover w-[80%] h-[35vh] mb-1 hover:scale-110 flex justify-center">
+                <img
+                  className="w-[90%] h-[150px] lg:h-[250px]"
+                  src={item.image}
+                />
+              </div>
+              <h1 className="font-semibold ">{item.title}</h1>
+            </Link>
+          ))}
       </div>
     </>
+  ) : (
+    <Loading />
   );
 }
 
