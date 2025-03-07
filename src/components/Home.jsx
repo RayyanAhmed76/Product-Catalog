63ab888191ff1bcc5c2edf6659301a849b09afb8
RayyanAhmed76 <rayyanahmedrakk@gmail.com>
2025-03-07 11:45:30 -0800

    changes in ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,15 +36,15 @@ function Home() {
   return products ? (
     <>
       <Nav />
-      <div className="w-[85%] p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 overflow-x-hidden overflow-y-auto mt-8 ">
+      <div className="  w-[85%]  p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 overflow-x-hidden overflow-y-auto  mt-8 ">
         {filteredproduct &&
           filteredproduct.map((item, index) => (
             <Link
               key={index}
               to={`/details/${item.id}`}
-              className="card flex flex-col items-center border border-black/10 rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow "
+              className=" h-[25vh] card flex flex-col items-center border border-black/10 rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow "
             >
-              <div className="w-full flex justify-center">
+              <div className="w-full  flex justify-center">
                 <img
                   className="w-full max-w-[200px] h-[150px] object-contain mb-4"
                   src={item.image}
